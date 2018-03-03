c9f0ea6219225d40f9ab41c56b5bf2cb3d93ae4e
hirakiuc <hirakiuc@gmail.com>
2018-03-03 13:51:20 +0900

    :shower: Remove logger

--- lib/ogpr/fetcher.rb
+++ lib/ogpr/fetcher.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 require 'uri'
-require_relative './logger.rb'
 require_relative './fetcher/html_fetcher.rb'
 
 module Ogpr
   class Fetcher
-    def self.fetch(url, options)
-      HtmlFetcher.new(url, options).fetch()
+    def self.fetch(url)
+      HtmlFetcher.new(url).fetch
     end
   end
 end
